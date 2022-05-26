a8aaa4c3dd9702e6bb627b4b58bdf171ff65edf0
claireWei <claire610323@gmail.com>
2022-05-26 13:22:43 -0700

    update CV & projects

--- src/assets/data/CV.js
+++ src/assets/data/CV.js
@@ -5,7 +5,7 @@ const CVData = {
     { "title": "Javascript", "score": 4},
     { "title": "Git & Github", "score": 4},
     { "title": "Vue.js", "score": 4},
-    { "title": "React.js", "score": 3},
+    { "title": "React.js", "score": 4},
     { "title": "RWD", "score": 3},
     { "title": "Typescript", "score": 2},
     { "title": "Phaser 3", "score": 2}
@@ -39,7 +39,16 @@ const CVData = {
   ],
   projects: [
     {
-      "id": 1, "title": "Full Stack Blog Website",
+      "id": 1, "title": "Full Stack Ecommerce Website",
+      "time": "2022.05",
+      "keywords": ["React", "Next.js", "Sanity", "PayPal"],
+      "link": "https://react-next-sanity-ecommerce-wei30172.vercel.app/",
+      "code": "https://github.com/wei30172/react-next-sanity-ecommerce",
+      "description": "Built a ecommerce website with React, Next.js, and Sanity. Users can place orders and see order history.",
+      "images": ["full-stack-ecommerce1", "full-stack-ecommerce2", "full-stack-ecommerce3", "full-stack-ecommerce4", "full-stack-ecommerce5", "full-stack-ecommerce6", "full-stack-ecommerce7", "full-stack-ecommerce8"]
+    },
+    {
+      "id": 2, "title": "Full Stack Blog Website",
       "time": "2022.03",
       "keywords": ["React", "Redux", "Next.js", "Tailwind CSS", "GraphQL"],
       "link": "https://react-next-blog-202203.netlify.app/",
@@ -48,7 +57,7 @@ const CVData = {
       "images": ["full-stack-web1", "full-stack-web2", "full-stack-web3", "full-stack-web4", "full-stack-web5"]
     },
     {
-      "id": 2, "title": "Full Stack MERN Website",
+      "id": 3, "title": "Full Stack MERN Website",
       "time": "2022.02",
       "keywords": ["React", "Redux", "Node.js", "Express", "MongoDB"],
       "link": "https://react-node-posts-202202.netlify.app/",
@@ -57,7 +66,7 @@ const CVData = {
       "images": ["MERN-web1", "MERN-web2", "MERN-web3", "MERN-web4", "MERN-web5", "MERN-web6"]
     },
     {
-      "id": 3, "title": "E-Commerce Shopping Cart",
+      "id": 4, "title": "E-Commerce Shopping Cart",
       "time": "2022.01",
       "keywords": ["React", "Redux", "TypeScript"],
       "link": "https://react-redux-ts-shoppingcart.vercel.app/",
@@ -66,7 +75,7 @@ const CVData = {
       "images": ["react-e-commerce1", "react-e-commerce2", "react-e-commerce3", "react-e-commerce4", "react-e-commerce5"]
     },
     {
-      "id": 4, "title": "Memory Game",
+      "id": 5, "title": "Memory Game",
       "time": "2021.11",
       "keywords": ["React"],
       "link": "https://memory-game-react-bay.vercel.app/",
@@ -75,7 +84,7 @@ const CVData = {
       "images": ["react-memory-game1", "react-memory-game2"]
     },
     {
-      "id": 5, "title": "React Recipe Web",
+      "id": 6, "title": "React Recipe Web",
       "time": "2021.11",
       "keywords": ["React", "Next"],
       "link": "https://recipe-next-contentful-three.vercel.app/",
@@ -84,7 +93,7 @@ const CVData = {
       "images": ["react-recipe-web1", "react-recipe-web2"]
     },
     {
-      "id": 6, "title": "React Bulletin Board",
+      "id": 7, "title": "React Bulletin Board",
       "time": "2021.10",
       "keywords": ["React", "Firebase"],
       "link": "https://react-project-web-a896d.web.app/",
@@ -93,7 +102,7 @@ const CVData = {
       "images": ["react-bulletin-board1", "react-bulletin-board2", "react-bulletin-board3", "react-bulletin-board4"]
     },
     {
-      "id": 7, "title": "React Reading List",
+      "id": 8, "title": "React Reading List",
       "time": "2021.10",
       "keywords": ["React"],
       "link": "https://booklist-context-hooks.vercel.app/",
@@ -102,7 +111,7 @@ const CVData = {
       "images": ["react-reading-list1", "react-reading-list2", "react-reading-list3"]
     },
     {
-      "id": 8, "title": "Snake Game",
+      "id": 9, "title": "Snake Game",
       "time": "2021.10",
       "keywords": ["HTML", "CSS", "JavaScript"],
       "link": "https://wei30172.github.io/Snake_Game/",
@@ -111,7 +120,7 @@ const CVData = {
       "images": ["snake-game1", "snake-game2", "snake-game3"]
     },
     {
-      "id": 9, "title": "Tic Tac Toe",
+      "id": 10, "title": "Tic Tac Toe",
       "time": "2021.10",
       "keywords": ["HTML", "CSS", "JavaScript"],
       "link": "https://wei30172.github.io/Tic_Tac_Toe/",
@@ -120,7 +129,7 @@ const CVData = {
       "images": ["tic-tac-toe1", "tic-tac-toe2", "tic-tac-toe3"]
     },
     {
-      "id": 10, "title": "E-Commerce Website",
+      "id": 11, "title": "E-Commerce Website",
       "time": "2020.04",
       "keywords": ["Vue", "Firebase", "Bootstrap"],
       "link": "https://vue-shop-3b5cc.web.app",
