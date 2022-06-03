b79fa1c212669d4f77e8f8d65ff53b59dcde86a3
claireWei <claire610323@gmail.com>
2022-06-03 15:40:52 -0700

    update CV

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Siao-Ping, Wei
-## A front-end engineer with one year of experience. Able to work with UI/UX designers and back-end engineers to create user interfaces using HTML5, CSS3, JavaScript, TypeScript, Vue, and React.
+## Experienced Frontend Developer. Able to work with UI/UX designers and back-end engineers to create user interfaces using HTML5, CSS3, JavaScript, TypeScript, Vue, and React.
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div className="about">
       <h2>About</h2>
       <p className="about-container">
-        A front-end engineer with one year of experience. Able to work with UI/UX designers and back-end engineers to create user interfaces using HTML5, CSS3, JavaScript, TypeScript, Vue, and React..
+        Experienced Frontend Developer. Able to work with UI/UX designers and back-end engineers to create user interfaces using HTML5, CSS3, JavaScript, TypeScript, Vue, and React..
       </p>
     </div>
   );
